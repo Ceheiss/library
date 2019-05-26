178f38d9a90e1877a67aebb0557f88ace0d89ce7
RicardoJacinto <ricardosj204@gmail.com>
2019-05-26 17:51:38 +0100

    added fade in effect to books

--- library.js
+++ library.js
@@ -1,7 +1,39 @@
+//On page load functions-----------------------------------------------------------------------------------------------------------------------------------------------------
+// hide div sections in order to load as you scroll down the page.
 
-var getNewBook = $("#new-book");
-
-//Toggle bookshelf 
-getNewBook.on('click', function(){
-    $('#form').slideToggle();
+$(document).ready(function(){
+    
+    var getNewBook = $("#new-book");
+    
+    //Toggle bookshelf 
+    getNewBook.on('click', function(){
+        $('#form').slideToggle();
+    });
 });
+
+//listen to the scroll of the window---------------------------------------------------------------------------------------------------------------------------------------------------
+$(window).scroll(function(e){
+
+ // This variable is very important as it tracks the window's scrollTop(); position and all animations are triggered based on this value
+ var scrollTop = $(this).scrollTop();
+
+ // Book list array
+var bookList = document.querySelectorAll(".book");
+var iteration = 0;
+
+		// Added this variable as a control method as somethimes the offset().top was set to 0 randomly and would conflict with scrollTop, because scrollTop > 0
+		if($('#bookshelf').offset().top !=0){
+            // When scrolltop is bigger than bookshelf heigh - 750, trigger animation
+			if(scrollTop > $('#bookshelf').offset().top + $('#bookshelf').height()-1000 ){ 	 
+                    //loop through booklist array
+					bookList.forEach(function(book){
+	            		// had a delay with setTimeout function in order to load pictures to the page with a sligth delay
+					    setTimeout(function(){
+			 		   	book.classList.add('show-book');
+			 		   }, (125*(iteration+1)));
+						iteration++;
+					});
+	             }  
+              }
+});
+
